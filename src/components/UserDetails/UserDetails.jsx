dcd93d5bb215264b727c675403c45dbb85fbf8ae
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-21 00:56:21 +0600

    go back button function added

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,12 +1,17 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const UserDetails = () => {
     const user = useLoaderData();
     const { name } = user;
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    };
     return (
         <div>
             <h2>User Name: {name}</h2>
+            <button className="btn" onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
